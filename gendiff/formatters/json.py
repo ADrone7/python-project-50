8bef911eed4e5ed51161bfd0da7981a938ec3074
ADrone <aleksandron1997@gmail.com>
2025-02-01 16:17:14 +0300

    Refactor json and stylish formatters

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -15,41 +15,41 @@ def convert(value) -> str:
     return value
 
 
+def walk_list(report: list, subdiff: list | tuple, depth: int = 0) -> None:
+    indent = (depth + 1) * INDENT_COUNT * INDENT
+    for i, data in enumerate(subdiff):
+        if isinstance(data, dict):
+            report.append(f'{indent}' + '{')
+            walk_dict(report, data, depth + 1)
+        elif isinstance(data, (list, tuple)):
+            report.append(f'{indent}' + '[')
+            walk_list(report, data, depth + 1)
+        else:
+            report.append(f'{indent}{convert(data)}')
+        if i == len(subdiff) - 1:
+            continue
+        report[-1] += ','
+    report.append(depth * INDENT_COUNT * INDENT + ']')
+
+
+def walk_dict(report: list, subdiff: dict, depth: int = 0) -> None:
+    indent = (depth + 1) * INDENT_COUNT * INDENT
+    for i, (key, data) in enumerate(sorted(subdiff.items())):
+        if isinstance(data, dict):
+            report.append(f'{indent}"{key}": ' + '{')
+            walk_dict(report, data, depth + 1)
+        elif isinstance(data, (list, tuple)):
+            report.append(f'{indent}"{key}": ' + '[')
+            walk_list(report, data, depth + 1)
+        else:
+            report.append(f'{indent}"{key}": {convert(data)}')
+        if i == len(subdiff) - 1:
+            continue
+        report[-1] += ','
+    report.append(depth * INDENT_COUNT * INDENT + '}')
+
+
 def format_json(diff: dict) -> str:
     report = ['{']
-    indent_base = INDENT_COUNT * INDENT
-        
-    def walk_list(subdiff: list | tuple, depth: int = 0) -> None:
-        indent = (depth + 1) * indent_base
-        for i, data in enumerate(subdiff):
-            if isinstance(data, dict):
-                report.append(f'{indent}' + '{')
-                walk_dict(data, depth + 1)
-            elif isinstance(data, (list, tuple)):
-                report.append(f'{indent}' + '[')
-                walk_list(data, depth + 1)
-            else:
-                report.append(f'{indent}{convert(data)}')
-            if i == len(subdiff) - 1:
-                continue
-            report[-1] += ','
-        report.append(depth * indent_base + ']')
-
-    def walk_dict(subdiff: dict, depth: int = 0) -> None:
-        indent = (depth + 1) * indent_base
-        for i, (key, data) in enumerate(sorted(subdiff.items())):
-            if isinstance(data, dict):
-                report.append(f'{indent}"{key}": ' + '{')
-                walk_dict(data, depth + 1)
-            elif isinstance(data, (list, tuple)):
-                report.append(f'{indent}"{key}": ' + '[')
-                walk_list(data, depth + 1)
-            else:
-                report.append(f'{indent}"{key}": {convert(data)}')
-            if i == len(subdiff) - 1:
-                continue
-            report[-1] += ','
-        report.append(depth * indent_base + '}')
-
-    walk_dict(diff)
+    walk_dict(report, diff, 0)
     return SEPARATOR.join(report)
--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -13,62 +13,46 @@ def convert(value) -> str:
     return value
 
 
+def process_data(report: list, data, key: str, 
+                 symbol: str = '', indent: str = '', depth: int = 0) -> None:
+    if isinstance(data, dict):
+        report.append(f"{indent}{symbol} {key}: " + '{')
+        walk_dict(report, data, depth + 1)
+    else:
+        report.append(f"{indent}{symbol} {key}: {convert(data)}")
+
+
+def walk_dict(report: list, subdiff: dict, depth: int = 0) -> None:
+    indent = (INDENT_COUNT * (depth + 1) - 2) * INDENT
+    for key, data in subdiff.items():
+        process_data(report, data, key, ' ', indent, depth)
+    report.append(depth * INDENT_COUNT * INDENT + '}')
+
+
+def walk_diff(report: list, subdiff: dict, depth: int = 0) -> None:
+    indent = (INDENT_COUNT * (depth + 1) - 2) * INDENT
+    for key, data in subdiff.items():
+        status = data["status"]
+        if status == "added":
+            process_data(report, data["value"], key, '+', indent, depth)
+            continue
+        if status == "removed":
+            process_data(report, data["value"], key, '-', indent, depth)
+            continue
+        if status == "unchanged":
+            process_data(report, data["value"], key, ' ', indent, depth)
+            continue
+        if status == "changed":
+            process_data(report, data["old_value"], key, '-', indent, depth)
+            process_data(report, data["new_value"], key, '+', indent, depth)
+            continue
+        if status == "nested change":
+            report.append(f"{indent}  {key}: " + '{')
+            walk_diff(report, data["value"], depth + 1)
+    report.append(depth * INDENT_COUNT * INDENT + '}')
+
+
 def format_stylish(diff: dict) -> str:
     report = ['{']
-
-    def walk_dict(subdiff: dict, depth: int = 0) -> None:
-        indent = (INDENT_COUNT * (depth + 1) - 2) * INDENT
-        for key, data in subdiff.items():
-            if isinstance(data, dict):
-                report.append(f"{indent}  {key}: " + '{')
-                walk_dict(data, depth + 1)
-                continue
-            report.append(f"{indent}  {key}: {convert(data)}")
-        report.append(depth * INDENT_COUNT * INDENT + '}')
-
-    def walk_diff(subdiff: dict, depth: int = 0) -> None:
-        indent = (INDENT_COUNT * (depth + 1) - 2) * INDENT
-        for key, data in subdiff.items():
-            status = data["status"]
-            if status == "added":
-                if isinstance(data["value"], dict):
-                    report.append(f"{indent}+ {key}: " + '{')
-                    walk_dict(data["value"], depth + 1)
-                else:
-                    report.append(f"{indent}+ {key}: {convert(data["value"])}")
-                continue
-            if status == "removed":
-                if isinstance(data["value"], dict):
-                    report.append(f"{indent}- {key}: " + '{')
-                    walk_dict(data["value"], depth + 1)
-                else:
-                    report.append(f"{indent}- {key}: {convert(data["value"])}")
-                continue
-            if status == "unchanged":
-                if isinstance(data["value"], dict):
-                    report.append(f"{indent}  {key}: " + '{')
-                    walk_dict(data["value"], depth + 1)
-                else:
-                    report.append(f"{indent}  {key}: {convert(data["value"])}")
-                continue
-            if status == "changed":
-                old = data["old_value"]
-                new = data["new_value"]
-                if isinstance(old, dict):
-                    report.append(f"{indent}- {key}: " + '{')
-                    walk_dict(old, depth + 1)
-                else:
-                    report.append(f"{indent}- {key}: {convert(old)}")
-                if isinstance(new, dict):
-                    report.append(f"{indent}+ {key}: " + '{')
-                    walk_dict(new, depth + 1)
-                else:
-                    report.append(f"{indent}+ {key}: {convert(new)}")
-                continue
-            if status == "nested change":
-                report.append(f"{indent}  {key}: " + '{')
-                walk_diff(data["value"], depth + 1)
-        report.append(depth * INDENT_COUNT * INDENT + '}')
-
-    walk_diff(diff)
+    walk_diff(report, diff)
     return SEPARATOR.join(report)
